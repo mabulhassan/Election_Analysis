f73c124f601b64db471c3f96c6e5e74bfd5ebe78
maha <maha3220@yahoo.com>
2023-01-10 21:40:00 -0500

    modify pypoll

--- PyPoll.py
+++ PyPoll.py
@@ -6,6 +6,10 @@ file_to_load = os.path.join("..","Downloads", "election_results.csv")
 # Assign a variable to save the file to a path.
 #file_to_save = os.path.join("..","Downloads", "election_analysis.txt")
 
+#add total votes counter
+total_votes=0
+candidate_options=[]
+candidate_votes={}
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -15,4 +19,23 @@ with open(file_to_load) as election_data:
     print(headers)
      # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+       
+  # 2. Add to the total vote count.
+        total_votes += 1
+     # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+           # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count
+    candidate_votes[candidate_name] += 1
+
+
+# Print the candidate vote dictionary.
+print(candidate_votes)
